68a674a4c4a276aa2cb29759719d0470d10f726c
Fabian Thorand <f.thorand@gmail.com>
2014-09-24 16:54:46 +0200

    C memory primitives

--- kernel/idrisrts/idris_stdfgn.h
+++ kernel/idrisrts/idris_stdfgn.h
@@ -25,4 +25,26 @@ int idris_time();
 
 void idris_forceGC();
 
+// other primitives
+
+#define SETMEM8(addr,val) ((*(int8_t*)(addr)) = (int8_t)(val))
+#define SETMEM16(addr,val) ((*(int16_t*)(addr)) = (int16_t)(val))
+#define SETMEM32(addr,val) ((*(int32_t*)(addr)) = (int32_t)(val))
+#define SETMEM64(addr,val) ((*(int64_t*)(addr)) = (int64_t)(val))
+
+#define SETARR8(base,off,val) (((int8_t*)(base))[off] = (int8_t)(val))
+#define SETARR16(base,off,val) (((int16_t*)(base))[off] = (int16_t)(val))
+#define SETARR32(base,off,val) (((int32_t*)(base))[off] = (int32_t)(val))
+#define SETARR64(base,off,val) (((int64_t*)(base))[off] = (int64_t)(val))
+
+#define GETMEM8(addr) (*(int8_t*)(addr))
+#define GETMEM16(addr) (*(int16_t*)(addr))
+#define GETMEM32(addr) (*(int32_t*)(addr))
+#define GETMEM64(addr) (*(int64_t*)(addr))
+
+#define GETARR8(base,off) ((int8_t*)(base))[off]
+#define GETARR16(base,off) ((int16_t*)(base))[off]
+#define GETARR32(base,off) ((int32_t*)(base))[off]
+#define GETARR64(base,off) ((int64_t*)(base))[off]
+
 #endif
